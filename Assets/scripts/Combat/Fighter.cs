234ad6804f48e5d039309401a1f75618f0692333
odrozhzhyn <oleksandr.drozhzhyn@motorsport.com>
2024-11-24 18:42:29 +0200

    add Hit animation when player in attack range

--- Assets/scripts/Combat/Fighter.cs
+++ Assets/scripts/Combat/Fighter.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 using RPG.Movement;
+using RPG.Core;
 
 namespace RPG.Combat
 {
-    public class Fighter : MonoBehaviour
+    public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 4f;
+        [SerializeField] float timeBeetwenAnnacks = 1f;
         Transform target;
+
+        float timeScinceLastAttack = 0;
         private void Update()
         {
+            timeScinceLastAttack += Time.deltaTime;
+            // print($"timeScinceLastAttack: {timeScinceLastAttack}");
             if (target != null)
             {
 
@@ -16,18 +22,30 @@ namespace RPG.Combat
                 bool isInRange = currentDistance < weaponRange;
                 if (!isInRange)
                 {
-                    print(currentDistance);
+                    // print(currentDistance);
                     GetComponent<Mover>().MoveTo(target.position);
                 }
                 else
                 {
                     GetComponent<Mover>().Stop();
                     // target = null;
+                    AttackBehaviour();
                 }
             }
         }
+
+        private void AttackBehaviour()
+        {
+            if (timeBeetwenAnnacks < timeScinceLastAttack)
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeScinceLastAttack = 0;
+            }
+        }
+
         public void Atack(CombatTarget combatTarget)
         {
+            GetComponent<ActionScheduler>().StartAction(this);
             // print("Bang bang");
             target = combatTarget.transform;
         }
@@ -35,6 +53,12 @@ namespace RPG.Combat
         public void Cancel()
         {
             target = null;
+        }
+
+        // Animation event
+        void Hit()
+        {
+
         }
     }
 
--- Assets/scripts/Movement/Mover.cs
+++ Assets/scripts/Movement/Mover.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
-using RPG.Combat;
+using RPG.Core;
 using UnityEngine;
 using UnityEngine.AI;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour
+    public class Mover : MonoBehaviour, IAction
     {
         [SerializeField] Transform targer;
 
@@ -45,7 +45,7 @@ namespace RPG.Movement
 
         public void StartMoveAction(Vector3 destination)
         {
-            GetComponent<Fighter>().Cancel();
+            GetComponent<ActionScheduler>().StartAction(this);
             MoveTo(destination);
         }
 
@@ -56,6 +56,10 @@ namespace RPG.Movement
             navMeshAgent.isStopped = false;
         }
 
+        public void Cancel()
+        {
+            Stop();
+        }
         public void Stop()
         {
             navMeshAgent.isStopped = true;
