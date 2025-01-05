30b10e297fde9a1c486cfc4b98f4bdeef0ce4c0f
odrozhzhyn <oleksandr.drozhzhyn@motorsport.com>
2025-01-05 16:51:20 +0200

    after loosing chase distance enemy return on position which was position when they start chasing player + add chase gizmos

--- Assets/scripts/Control/AIController.cs
+++ Assets/scripts/Control/AIController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Control
@@ -9,13 +10,18 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         Fighter fighter;
+        Mover mover;
         Health health;
         GameObject player;
+
+        Vector3 guardPosition;
         [SerializeField] float chaseDistance = 8f;
         // Start is called before the first frame update
         void Start()
         {
+            guardPosition = transform.position;
             fighter = GetComponent<Fighter>();
+            mover = GetComponent<Mover>();
             health = GetComponent<Health>();
             player = GameObject.FindWithTag("Player");
         }
@@ -31,7 +37,8 @@ namespace RPG.Control
             }
             else
             {
-                fighter.Cancel();
+                // fighter.Cancel();
+                mover.StartMoveAction(guardPosition);
             }
         }
 
@@ -42,5 +49,23 @@ namespace RPG.Control
             print($"{gameObject.name} should chase because distance is  below chaseDistance and equal :{distanceToPlayer}");
             return distanceToPlayer < chaseDistance;
         }
+
+
+        /// ////////////////////////////////////////////////////////////////////
+
+        // Called by Unity
+
+        // private void OnDrawGizmos()
+        // {
+
+        // }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        }
+
+        /// ////////////////////////////////////////////////////////////////////
     }
 }
