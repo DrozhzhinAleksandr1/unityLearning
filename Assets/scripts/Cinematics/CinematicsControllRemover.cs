91a3ecea177732df80757025e7cfafa25bdad859
odrozhzhyn <oleksandr.drozhzhyn@motorsport.com>
2025-02-28 10:39:50 +0200

    disable control when start cut scene and enable when finished / add Scene2

--- Assets/scripts/Cinematics/CinematicsControllRemover.cs
+++ Assets/scripts/Cinematics/CinematicsControllRemover.cs
@@ -2,26 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using RPG.Core;
+using RPG.Control;
 
 namespace RPG.Sinematics
 {
     public class CinematicsControllRemover : MonoBehaviour
     {
+        GameObject player;
         private void Start()
         {
+            GameObject player = GameObject.FindWithTag("Player");
             GetComponent<PlayableDirector>().played += DisableControll;
             GetComponent<PlayableDirector>().stopped += EnableControll;
         }
 
         void DisableControll(PlayableDirector pd)
         {
-            print("DisableControll");
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerControler>().enabled = false;
         }
 
         void EnableControll(PlayableDirector pd)
         {
-            print("EnableControll");
-
+            player.GetComponent<PlayerControler>().enabled = true;
         }
     }
 
