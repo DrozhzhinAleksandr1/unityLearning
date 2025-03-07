077523b48d45c8583d628053e04449dd7345a51d
odrozhzhyn <oleksandr.drozhzhyn@motorsport.com>
2025-03-07 09:23:17 +0200

    create fader

---
+++ Assets/scripts/SceneManagment/Fader.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagment
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvasGroup;
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+            // StartCoroutine(FadeOutIn());
+        }
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1)
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
+        IEnumerator FadeOutIn()
+        {
+            yield return FadeOut(3f);
+            print("fade out");
+            yield return FadeIn(3f);
+            print("fade in");
+
+        }
+
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0)
+            {
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+            }
+        }
+    }
+}
