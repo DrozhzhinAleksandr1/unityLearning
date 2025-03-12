bcd5acf42f88a24da2acc5b32dc7551043133805
odrozhzhyn <oleksandr.drozhzhyn@motorsport.com>
2025-03-12 09:31:56 +0200

    restore hp and dead

--- Assets/scripts/Core/Health.cs
+++ Assets/scripts/Core/Health.cs
@@ -1,15 +1,20 @@
+using RPG.Saving;
 using UnityEngine;
 
 namespace RPG.Core
 {
-    public class Health : MonoBehaviour
+    public class Health : MonoBehaviour, ISaveable
     {
         [SerializeField] float healthPoints = 100f;
         bool isDead = false;
+
+
         public bool IsDead()
         {
             return isDead;
         }
+
+
         public void TakeDamage(float damage)
         {
             healthPoints = Mathf.Max(healthPoints - damage, 0);
@@ -26,6 +31,18 @@ namespace RPG.Core
             isDead = true;
             GetComponent<Animator>().SetTrigger("die");
             GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+        public object CaptureState()
+        {
+            return healthPoints;
+        }
+        public void RestoreState(object state)
+        {
+            healthPoints = (float)state;
+            if (healthPoints == 0)
+            {
+                Die();
+            }
         }
     }
 
