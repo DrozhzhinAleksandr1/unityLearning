46a12adf9f5a9a7de5859503a7642fe5182bd13a
odrozhzhyn <oleksandr.drozhzhyn@motorsport.com>
2025-06-23 13:52:43 +0300

    add increasing damage based on current lvl

--- Assets/scripts/Combat/Fighter.cs
+++ Assets/scripts/Combat/Fighter.cs
@@ -4,10 +4,12 @@ using RPG.Core;
 using RPG.Saving;
 using RPG.Attributes;
 using RPG.Stats;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace RPG.Combat
 {
-    public class Fighter : MonoBehaviour, IAction, ISaveable
+    public class Fighter : MonoBehaviour, IAction, ISaveable, IModifierProvider
     {
         [SerializeField] float timeBeetwenAnnacks = 1f;
         [SerializeField] Transform rightHandTransform = null;
@@ -131,6 +133,14 @@ namespace RPG.Combat
             GetComponent<Animator>().SetTrigger("stopAttack");
         }
 
+        public IEnumerable<float> GetAdditiveModifier(Stat stat)
+        {
+            if (stat == Stat.Damage)
+            {
+                yield return currentWeapon.getWeaponDamage();
+            }
+        }
+
         public object CaptureState()
         {
             return currentWeapon.name;
@@ -142,6 +152,7 @@ namespace RPG.Combat
             Weapon weapon = Resources.Load<Weapon>(weaponName);
             EquipWeapon(weapon);
         }
+
     }
 
 }
--- Assets/scripts/Stats/BaseStats.cs
+++ Assets/scripts/Stats/BaseStats.cs
@@ -47,7 +47,7 @@ namespace RPG.Stats
 
         public float GetStat(Stat stat)
         {
-            return progression.GetStat(stat, characterClass, GetLevel());
+            return progression.GetStat(stat, characterClass, GetLevel()) + GetAdditiveModifier(stat);
         }
 
         public int GetLevel()
@@ -57,6 +57,19 @@ namespace RPG.Stats
                 currentLevel = CalculateLevel();
             }
             return currentLevel;
+        }
+
+        private float GetAdditiveModifier(Stat stat)
+        {
+            float total = 0;
+            foreach (IModifierProvider provider in GetComponents<IModifierProvider>())
+            {
+                foreach (float modifier in provider.GetAdditiveModifier(stat))
+                {
+                    total += modifier;
+                }
+            }
+            return total;
         }
 
         public int CalculateLevel()
@@ -76,5 +89,6 @@ namespace RPG.Stats
             }
             return penultimateLevel + 1;
         }
+
     }
 }
